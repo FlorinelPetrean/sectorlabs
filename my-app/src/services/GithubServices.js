1fcd3b4ffb66340c604098c102ff0f9538a957af
florin.petrean <florin.petrean98@gmail.com>
2022-10-13 18:19:40 +0300

    Created gist api service

---
+++ my-app/src/services/GithubServices.js
@@ -0,0 +1,26 @@
+// import { Octokit } from "@octokit/core"
+import axios from "axios";
+
+// const octokit = new Octokit({ auth: "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" });
+const axiosAuth = axios.create({ 
+    headers: { 
+      Authorization: "Bearer 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"
+    } 
+  });
+const BASE_URL = 'https://api.github.com'
+
+class GithubServices {
+
+getUserGists(username) {
+    let response = axiosAuth.get(`${BASE_URL}/users/${username}/gists`)
+    return response;
+
+}
+
+getGistForks(gist_id) {
+    let response = axiosAuth.get(`${BASE_URL}/gists/${gist_id}/forks`)
+    return response;
+}
+
+
+} export default new GithubServices()
